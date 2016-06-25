1cf2316d739c4baf1631546d382f183d4389b414
Per Sandström <per.j.sandstrom@gmail.com>
2016-06-25 20:57:39 +0200

    lint pass

--- vasttrafik/client.py
+++ vasttrafik/client.py
@@ -1,7 +1,11 @@
-import requests
+"""
+Västtrafik API
+"""
+
 import base64
 import json
 import time as time_module
+import requests
 
 TOKEN_URL = 'https://api.vasttrafik.se/token'
 API_BASE_URL = 'https://api.vasttrafik.se/bin/rest.exe/v2'
@@ -9,86 +13,95 @@ CONSUMER_KEY = '<insert your CONSUMER_KEY here>'
 CONSUMER_SECRET = '<insert your CONSUMER_SECRET here>'
 
 
-def fetchToken(key, secret):
-	headers = {
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Authorization': 'Basic ' + base64.b64encode((key + ':' + secret).encode()).decode()
-	}
-	data = {'grant_type': 'client_credentials'}
+def fetch_token(key, secret):
+    """ Get token from key and secret """
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + base64.b64encode(
+            (key + ':' + secret).encode()).decode()
+        }
+    data = {'grant_type': 'client_credentials'}
 
-	response = requests.post(TOKEN_URL, data=data, headers=headers)
-	obj = json.loads(response.content.decode('UTF-8'))
-	return obj['access_token']
+    response = requests.post(TOKEN_URL, data=data, headers=headers)
+    obj = json.loads(response.content.decode('UTF-8'))
+    return obj['access_token']
+
 
 class Client:
+    """ Journy planner class"""
 
-	def __init__(self, format, key=CONSUMER_KEY, secret=CONSUMER_SECRET):
-		self.token = fetchToken(key, secret)
-		if format == 'JSON':
-			self.format = '&format=json'
-		else:
-			# default is XML
-			self.format = ''
+    def __init__(self, response_format='', key=CONSUMER_KEY,
+                 secret=CONSUMER_SECRET):
+        self.token = fetch_token(key, secret)
+        self.format = '&format=json' if response_format == 'JSON' else ''
 
-	## /journeyDetail endpoint
-	def get_journey_detail(self, ref, query_params=None):
-		return ''
+    # def get_journey_detail(self, ref, query_params=None):
+    #     """ /journeyDetail endpoint """
+    #     return ''
 
-	## /location endpoint
-	# /location.allstops
-	def get_all_stops(self, query_params=None):
-		raise Exception('Error: Can\'t get this endpoint to work. No idea why. Should be implemented in future.')
+    # /location endpoint
+    # /location.allstops
+    # def get_all_stops(self, query_params=None):
+    #     raise Exception("Error: Can't get this endpoint to work."
+    #                     " No idea why. Should be implemented in future.")
 
-	# /location.nearbystops
-	def get_nearby_stops(self, lat, long, query_params=None):
-		data = self.get('/location.nearbystops?originCoordLat=' + str(lat) + '&originCoordLong=' + str(long))
-		return data['LocationList']['StopLocation']
+    def get_nearby_stops(self, lat, long):
+        """ /location.nearbystops """
+        data = self.get(
+            '/location.nearbystops?originCoordLat=' + str(lat) +
+            '&originCoordLong=' + str(long))
+        return data['LocationList']['StopLocation']
 
-	# /location.name
-	def get_stops_by_name(self, query, query_params=None):
-		data = self.get('/location.name?input=' + query, query_params)
-		return data['LocationList']['StopLocation']
+    def get_stops_by_name(self, query, query_params=None):
+        """ /location.name """
+        data = self.get('/location.name?input=' + query, query_params)
+        return data['LocationList']['StopLocation']
 
-	# /location.nearbyaddress
-	def get_nearby_address(self, lat, long, query_params=None):
-		data = self.get('/location.name?input=' + query, query_params)
-		return data['LocationList']['CoordLocation']
+    # /location.nearbyaddress
+    # def get_nearby_address(self, lat, long, query_params=None):
+    #     data = self.get('/location.name?input=' + query, query_params)
+    #     return data['LocationList']['CoordLocation']
 
-	## /arrivalBoard endpoint
-	def get_arrivals(self, stopID, date=None, time=None, query_params=None):
-		if date is not None and time is not None:
-			data = self.get('/arrivalBoard?id=' + str(stopID) + '&date=' + date + '&time=' + time, query_params)
-		else:
-			data = self.get('/arrivalBoard?id=' + str(stopID) + '&date=' + time_module.strftime("%Y-%m-%d") + 
-			'&time=' + time_module.strftime("%H:%M"), query_params)
-		return data['ArrivalBoard']['Arrival']
+    def get_arrivals(self, stop_id, date=None, time=None, query_params=None):
+        """ /arrivalBoard endpoint """
+        if date is not None and time is not None:
+            data = self.get('/arrivalBoard?id=' + str(stop_id) + '&date=' +
+                            date + '&time=' + time, query_params)
+        else:
+            data = self.get('/arrivalBoard?id=' + str(stop_id) + '&date=' +
+                            time_module.strftime("%Y-%m-%d") +
+                            '&time=' + time_module.strftime("%H:%M"),
+                            query_params)
+        return data['ArrivalBoard']['Arrival']
 
-	## /departureBoard endpoint
-	def get_departures(self, stopID, date=None, time=None, query_params=None):
-		if date is not None and time is not None:
-			data = self.get('/departureBoard?id=' + str(stopID) + '&date=' + date + '&time=' + time, query_params)
-		else:
-			data = self.get('/departureBoard?id=' + str(stopID) + '&date=' + time_module.strftime("%Y-%m-%d") + 
-			'&time=' + time_module.strftime("%H:%M"), query_params)
-		return data['DepartureBoard']['Departure']
+    def get_departures(self, stop_id, date=None, time=None, query_params=None):
+        """ /departureBoard endpoint """
+        if date is not None and time is not None:
+            data = self.get('/departureBoard?id=' + str(stop_id) +
+                            '&date=' + date +
+                            '&time=' + time, query_params)
+        else:
+            data = self.get('/departureBoard?id=' + str(stop_id) +
+                            '&date=' + time_module.strftime("%Y-%m-%d") +
+                            '&time=' + time_module.strftime("%H:%M"),
+                            query_params)
+            return data['DepartureBoard']['Departure']
 
-	## /trip endpoint
-	def calculate_trip(self, query_params=None):
-		return ''
+    # def calculate_trip(self, query_params=None):
+    #     """ trip endpoint """
+    #     return ''
 
-	## request builder
-	def get(self, endpoint, query_params=None):
-		url = API_BASE_URL + endpoint + self.format		
+    def get(self, endpoint, query_params=None):
+        """ request builder """
+        url = API_BASE_URL + endpoint + self.format
+        if query_params is not None:
+            for key in query_params:
+                url += '&' + key + '=' + query_params[key]
 
-		if query_params is not None:
-			for key in query_params:
-				url += '&' + key + '=' + query_params[key]
-
-		headers = {
-			'Authorization': 'Bearer ' + self.token
-		}
-		res = requests.get(url, headers=headers)
-		if res.status_code == 200:
-			return json.loads(res.content.decode('UTF-8'), 'UTF-8')
-		else:
-			raise Exception('Error: ' + str(res.status_code) + str(res.content))
+        headers = {'Authorization': 'Bearer ' + self.token}
+        res = requests.get(url, headers=headers)
+        if res.status_code == 200:
+            return json.loads(res.content.decode('UTF-8'), 'UTF-8')
+        else:
+            raise Exception('Error: ' + str(res.status_code) +
+                            str(res.content))
